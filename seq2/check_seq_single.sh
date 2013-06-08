ecb8b76e3107c132c2ceeec5a85b880cba88fdb9
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2013-06-08 20:00:00 +0200

    fix

--- seq2/check_seq_single.sh
+++ seq2/check_seq_single.sh
@@ -9,6 +9,8 @@ number_shifted=$(($SHIFT+$number))
 ours=`printf 'image_%05d.xml' $number_shifted`
 theirs=`printf 'image_%05d.xml' $number`
 
+diff $ours ../../../seq2-original.seq/$theirs
+
 if [ 0 == `diff $ours ../../../seq2-original.seq/$theirs | grep 13c13 | wc -l` ] ; then
     printf "\033[1;32m%05d : ok\n" $number
 else
@@ -24,5 +26,5 @@ SUM_THEIRS=`md5sum -b ../../../seq2-original.seq/$theirs | cut -b-32`
 if [ $SUM_OURS == $SUM_THEIRS ] ; then
     printf "\033[1;32m%05d : sum ok ($SUM_OURS)\n" $number
 else
-    printf "\033[1;31m%05d : sum fail ($SUM_OURS\n" $number
+    printf "\033[1;31m%05d : sum fail ($SUM_OURS)\n" $number
 fi
